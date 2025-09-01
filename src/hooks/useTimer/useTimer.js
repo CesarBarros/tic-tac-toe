ddddf7f2ee279bb9930dcc185961dde9282bd616
Cesar Filho <cesarbsfilho@yahoo.com.br>
2025-09-01 15:32:57 -0300

    docs: add JSDoc to useTimer hook

--- src/hooks/useTimer/useTimer.js
+++ src/hooks/useTimer/useTimer.js
@@ -1,5 +1,16 @@
 import { useEffect, useState } from "react";
 
+/**
+ * Custom hook for managing a countdown timer in the game.
+ *
+ * @param {()=> void} onTimeOut - Callback function to execute when the timer reaches zero.
+ * @param {boolean} gameOver - Indicates if the game is over.
+ * @param {string} currentPlayer - The current player, used to reset the timer on player change.
+ * @returns {{
+ *   timeLeft: number,
+ *   isActive: boolean,
+ * }} An object containing the remaining time and timer activity status.
+ */
 export const useTimer = (onTimeOut, gameOver, currentPlayer) => {
   const [timeLeft, setTimeLeft] = useState(5);
   const [isActive, setIsActive] = useState(true);
